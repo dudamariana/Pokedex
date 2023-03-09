372d10345d7667c07e63bb756be7dfbc2f857464
dudamariana <maria.botura@etec.sp.gov.br>
2023-03-09 11:41:09 -0300

    Adicionado serviço de sessão e criado as interfaces e classes de serviço

---
+++ Pokedex/Services/PokeService.cs
@@ -0,0 +1,53 @@
+using System.Text.Json;
+using Pokedex.Models;
+namespace Pokedex.Services;
+
+public class PokeService : IPokeService
+{
+    private readonly IHttpContextAccessor _session;
+
+    public PokeService(IHttpContextAccessor session)
+    {
+        _session = session;
+        PopularSessao();
+    }
+
+    public Pokemon GetPokemon(int Numero)
+    {
+
+     }
+    public List<Pokemon> GetPokemons()
+    {
+        PopularSessao();
+   
+        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(
+            LerArquivo(@"Data\pokemons.json")
+        );
+     }
+    public List<Tipo> GetTipos()
+    {
+                     var tipos = JsonSerializer.Deserialize<List<Tipo>>(
+           LerArquivo(@"Data\tipos.json")
+        );
+     } 
+
+   private void PopularSessao()
+   {
+    
+        if (string.IsNullOrEmpty(_session.HttpContext.Session.GetString("Tipos")))
+        {
+            _session.HttpContext.Session.SetString("Tipos", LerArquivo(@"Data\tipos.json"));
+            _session.HttpContext.Session.SetString("Pokemons", LerArquivo(@"Data\pokemons.json"));
+        }
+        
+   }
+
+     private string LerArquivo(string nomeArquivo)
+    {
+        using (StreamReader leitor = new StreamReader(nomeArquivo))
+        {
+            string dados = leitor.ReadToEnd();
+            return dados;
+        }
+    }
+}
