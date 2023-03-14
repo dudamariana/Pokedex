63e5a1b94abce5ff9aef8caf3941fa8cce9f36a3
dudamariana <maria.botura@etec.sp.gov.br>
2023-03-14 11:40:05 -0300

    Finalizado

--- Pokedex/Services/IPokeService.cs
+++ Pokedex/Services/IPokeService.cs
@@ -6,4 +6,7 @@ public interface IPokeService
     Pokemon GetPokemon(int Numero);
     List<Pokemon> GetPokemons();
     List<Tipo> GetTipos();
+    PokedexDto GetPokedexDto();
+    DetailsDto GetDetailedPokemon(int Numero);
+    Tipo GetTipo(string Nome);
 }
